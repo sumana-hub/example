45dd9797fdf009ec06e66e2d4a4d62c2a0cf678c
sumana-hub <sumana.dhital@triosstudent.com>
2024-11-13 17:19:11 -0500

    End of Chapter 13

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductComponent } from './component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { PaAttrDirective } from "./attr.directive";
+import { PaModel } from "./twoway.directive";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ProductComponent, PaAttrDirective, PaModel],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProductComponent]
 })
 export class AppModule { }
